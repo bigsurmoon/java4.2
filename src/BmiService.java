3be04359e6f151993622ee91d79d78c34e8f31e7
bigsurmoon <vinny4000@gmail.com>
2021-09-28 01:18:04 +0700

    init commit

---
+++ src/BmiService.java
@@ -0,0 +1,12 @@
+public class BmiService {
+    public double calculate(double weight, double height) {
+
+        double massIndex = weight / (height * height);
+
+        return massIndex;
+    }
+
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+        double massIndex = service.calculate(77, 1.70); // Вес писать в килограммах, рост в метрах!!!
+        System.out.println("Индекс массы: " + massIndex);
+
+
+    }
+}
