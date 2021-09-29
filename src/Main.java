aefe7d33fb91c55e6e6de7f937f70d658875a985
Ed <85837973+bigsurmoon@users.noreply.github.com>
2021-09-29 16:18:17 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-
         double massIndex = service.calculate(77, 1.70); // Вес писать в килограммах, рост в метрах!!!
         System.out.println("Индекс массы: " + massIndex);
     }
